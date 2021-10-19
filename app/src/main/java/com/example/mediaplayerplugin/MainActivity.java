a15f8a9633749b4022cc254efbacc7a93ebe5936
Adamu Muktar <mkhtradm01@gmail.com>
2021-10-19 11:02:32 +0400

    initial commit

---
+++ app/src/main/java/com/example/mediaplayerplugin/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.mediaplayerplugin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button start, stop;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        start = (Button) findViewById(R.id.startButton);
+        stop = (Button) findViewById(R.id.stopButton);
+
+        start.setOnClickListener(this);
+        stop.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == start){
+            startService(new Intent(this, MediaPlayerService.class));
+        }else if(view == stop){
+            stopService(new Intent(this, MediaPlayerService.class));
+        }
+    }
+}
